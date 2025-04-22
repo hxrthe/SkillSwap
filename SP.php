79368026ff720339d1c1dd167fe054c1cad62670
Dharzzzz <pamaylaondarence153@gmail.com>
2025-04-22 15:18:40 +0800

    Add files via upload

--- SP.php
+++ SP.php
@@ -9,14 +9,31 @@ class Crud {
         $this->conn = $db->getConnection();
     }
 
-    public function createUser($firstname, $lastname, $email, $password) {
-        $stmt = $this->conn->prepare("CALL createUser(:first_name, :last_name, :email, :password)");
-        $stmt->execute([
-            ':first_name' => $firstname,
-            ':last_name' => $lastname,
-            ':email' => $email,
-            ':password' => $password // not hashed yet
-        ]);
+    public function createUser($firstname, $lastname, $email, $password, $verificationCode, $isVerified = false) {
+        try {
+            // Call the stored procedure
+            $stmt = $this->conn->prepare("CALL CreateUser(?, ?, ?, ?, ?, ?)");
+            
+            $stmt->execute([
+                $firstname,      // p_firstname
+                $lastname,       // p_lastname
+                $email,         // p_email
+                $password,      // p_password
+                $verificationCode, // p_verification_code
+                $isVerified ? 1 : 0  // p_is_verified
+            ]);
+
+        } catch (PDOException $e) {
+            // Check for the custom error from the stored procedure
+            if ($e->getCode() == '45000') {
+                throw new Exception('email_exists');
+            } else {
+                throw $e;
+            }
+        }    
     }
+
+    
+    
 }
 ?>
